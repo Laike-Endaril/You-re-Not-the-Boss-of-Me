23916a86f8d7701bf25fb27640bd3fe82ab98251
Kyle Koder <silvertallon@gmail.com>
2019-03-18 20:55:19 -0700

    1.12.2.002

--- src/main/java/com/fantasticsource/yourenotthebossofme/YoureNotTheBossOfMe.java
+++ src/main/java/com/fantasticsource/yourenotthebossofme/YoureNotTheBossOfMe.java
@@ -15,7 +15,7 @@ public class YoureNotTheBossOfMe
 {
     public static final String MODID = "yourenotthebossofme";
     public static final String NAME = "You're Not the Boss of Me";
-    public static final String VERSION = "1.12.2.001";
+    public static final String VERSION = "1.12.2.002";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
